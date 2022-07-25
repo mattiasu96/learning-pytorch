3e75bc933bc27e0dbe562eb54f278629bc61d92d
mattiasu96 <mattiasu96@live.it>
2022-07-25 22:36:14 +0200

    Temp notes for transfer learning

---
+++ Deep Learning with Pytorch full course/12-transfer_learning.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+device = torch.device("msp" if torch.has_mps else "cpu")
+
+mean = np.array([0.485, 0.456, 0.406])
+std = np.array([0.229, 0.224, 0.225])
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
+}
+
+# import data
+
+data_dir = '.data/hymenoptera_data'
+sets = ['train', 'val']
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val']}
